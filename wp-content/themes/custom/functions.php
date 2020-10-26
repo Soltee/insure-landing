c670ac28098a70a521e0f9ada1d0b18b8427160a
Soltee <soltee.13@gmail.com>
2020-10-26 21:09:26 +0545

    added fonts

--- wp-content/themes/custom/functions.php
+++ wp-content/themes/custom/functions.php
@@ -2,7 +2,6 @@
 
 /*Styles & Scripts*/
 function insure_init() {
-
 	$theme_version = wp_get_theme()->get( 'Version' );
 	wp_enqueue_style( 'insure-style', get_stylesheet_uri(), array(), $theme_version );
 	wp_enqueue_style( 'insure-normalize', get_template_directory_uri() . '/assets/css/normalize.css', null, $theme_version );
